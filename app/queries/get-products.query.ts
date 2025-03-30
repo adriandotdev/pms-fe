d3cd9f59081d2612851d675de5a2a201a13151fc
Nads <63532775+adriandotdev@users.noreply.github.com>
2025-03-30 20:34:04 +0800

    implement sorting by id (#7)

--- app/queries/get-products.query.ts
+++ app/queries/get-products.query.ts
@@ -1,3 +1,4 @@
+import { SortingState } from "@tanstack/react-table";
 import axios from "axios";
 export interface Product {
 	id: number;
@@ -19,17 +20,19 @@ export interface QueryResponse {
 	totalProducts: number;
 }
 
-export const getProducts = async ({
-	pageSize,
-	pageIndex,
-}: {
-	pageSize: number;
-	pageIndex: number;
-}) => {
+export const getProducts = async (
+	pagination: { pageSize: number; pageIndex: number },
+	sorting: SortingState
+) => {
+	const { pageSize, pageIndex } = pagination;
+
+	// Ensure sorting is not empty before accessing sorting[0]
+	const sortDirection = sorting.length > 0 && sorting[0].desc ? "desc" : "asc";
+
 	const response = await axios.get<QueryResponse>(
 		`http://localhost:5126/api/v1/products?pageSize=${pageSize}&pageNumber=${
 			pageIndex + 1
-		}`
+		}${sorting.length > 0 ? `&sortDirection=${sortDirection}` : ""}`
 	);
 
 	return response.data;
